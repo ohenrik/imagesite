7f1b7878137fa534ffcfa6c0640720750a508b53
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2013-10-30 19:52:26 +0100

    Added basic working templating with Liquid, based on a file in public directory

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
 
+
+
+def liquidize(model_content)
+content = File.read "#{Rails.root}/public/test/index.liquid"
+#content = "This image is named " + @photo.name
+  Liquid::Template.parse(content).render(model_content)
+end
+
+
 end
