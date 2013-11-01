87deba864b88fc3bca91059b3030de87a09d5bbd
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2013-11-01 17:32:24 +0100

    Added subdomain spesific theme, by modifying liquidize helper method to incorporate subdomain into theme path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,13 @@ module ApplicationHelper
 
 
 
-def liquidize(model_content, view = nil)
-content = File.read "#{Rails.root}/public/test/#{view}.liquid"
-#content = "This image is named " + @photo.name
-  Liquid::Template.parse(content).render(model_content)
+def liquidize(model_content, view = nil, content = nil)
+	
+		content = File.read "#{Rails.root}/public/themes/#{request.subdomain}/basic/#{view}.liquid" if content == nil
+	
+	
+	Liquid::Template.parse(content).render(model_content)
+
 end
 
 
