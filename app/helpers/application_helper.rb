019eda9a86835a66c283a89eb7f2963a1912b680
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2013-11-02 22:16:01 +0100

    moved theme files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,7 @@ module ApplicationHelper
 
 def liquidize(model_content, view = nil, content = nil)
 	
-		content = File.read "#{Rails.root}/public/themes/#{request.subdomain}/basic/templates/#{view}.liquid" if content == nil
-	
+	content = File.read "#{Rails.root}/public/themes/basic/templates/#{view}.liquid" if view != nil
 	
 	Liquid::Template.parse(content).render(model_content)
 
