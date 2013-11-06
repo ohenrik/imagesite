9ccc0a28231dffd0b8999df38fb39e09f1fdbe7b
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2013-11-06 18:50:17 +0100

    Removed current theme folder from users. Cleaned up old way of identifying current theme

---
+++ db/migrate/20131106174834_remove_current_theme_folder_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveCurrentThemeFolderFromUsers < ActiveRecord::Migration
+  def change
+  	remove_column :users, :current_theme_folder if table_exists? :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131105201538) do
+ActiveRecord::Schema.define(version: 20131106174834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,7 +86,6 @@ ActiveRecord::Schema.define(version: 20131105201538) do
     t.datetime "password_reset_sent_at"
     t.string   "auth_token"
     t.string   "username"
-    t.string   "current_theme_folder"
     t.integer  "theme_id"
   end
 
