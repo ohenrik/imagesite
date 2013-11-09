a4785eb9380551c304e8543f657ec34f1bde1826
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2013-11-09 19:50:24 +0100

    Cleaned up after changing theme upload logic. Also deleted theme assets test files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,17 +6,13 @@ module ApplicationHelper
 	def liquidize(model_content, view = nil, content = nil)
 
 
-		theme_root_path = "#{Rails.root}/public/uploads/themes/#{current_user.subdomain}/#{current_user.theme.id}/theme_files"
+		theme_root_path = "#{Rails.root}/themes/#{current_user.subdomain}/current_theme"
 		theme_file = "/templates/#{view}.html.liquid"
 		content = File.read(theme_root_path + theme_file)
 
-
-
-
 		# Load the base theme file system 
-		Liquid::Template.file_system =  Liquid::LocalFileSystem.new(Rails.root.to_s + "/public/uploads/themes/#{current_user.subdomain}/#{current_user.theme.id}/theme_files")
+		Liquid::Template.file_system =  Liquid::LocalFileSystem.new(theme_root_path)
 		
-
 
 		#file_system = Liquid::LocalFileSystem.new(content)
 		Liquid::Template.parse(content).render(model_content, :filters => [LiquidFilters])
--- app/helpers/themes_helper.rb
+++ app/helpers/themes_helper.rb
@@ -2,7 +2,7 @@ module ThemesHelper
 
 
 	def theme_preview_image(theme)
-		File.join(File.dirname(theme.zip_url), "preview.png").to_s
+		File.join(File.dirname(theme.zip_url), "preview.png").to_s if theme
 	end
 
 
