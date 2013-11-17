f4d5b5b740b6b3240d2c04f0c5465cc3d96ae523
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2013-11-17 20:03:32 +0100

    Added image_tag to liquids tag helpers

--- app/helpers/liquid_tags_helper.rb
+++ app/helpers/liquid_tags_helper.rb
@@ -81,7 +81,6 @@ module LiquidTagsHelper
 	    def render(context)
 
 	    	@path = Liquid::Template.file_system.root
-	    	css_file = "style.css"
 	    	
 	    	#derrive request subdomain from Liquid filesystem! 
 	    	@path_array = @path.to_s.split("/")
@@ -95,5 +94,40 @@ module LiquidTagsHelper
 	end
 
 	Liquid::Template.register_tag('css_tag', CssTag)
+
+
+	#
+	# image_tag
+	#
+	class ImageTag < Liquid::Tag
+
+		# Include the stylesheet tag link helper
+		include ActionView::Helpers::AssetTagHelper
+
+		def initialize(tag_name, image_attributes, tokens)
+			@image_attributes = image_attributes.tr('"', '').split(",")
+			@image_path = @image_attributes[0]
+			@alt = @image_attributes[1]
+			@title = @image_attributes[2]
+			@width = @image_attributes[3]
+			@height = @image_attributes[4]
+			super
+		end
+
+	    def render(context)
+
+	    	@path = Liquid::Template.file_system.root
+	    	
+	    	#derrive request subdomain from Liquid filesystem! 
+	    	@path_array = @path.to_s.split("/")
+	    	@subdomain = @path_array[-2]
+
+	    	image_tag("/assets/#{@subdomain}/images/#{@image_path.strip}", alt: @alt.strip, title: @title.strip, height: @height.try(:strip), width: @width.try(:strip))
+
+	    end	
+
+	end
+
+	Liquid::Template.register_tag('image_tag', ImageTag)
 
 end
