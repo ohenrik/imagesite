ad58d09bdc355a7e843b7c2ac9af282442be6fd4
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2014-03-25 20:05:31 +0100

    Small change to deploy.rd. Added symlink creation again

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,18 +30,14 @@ after "deploy:restart", "deploy:cleanup"
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :symlink_shared do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"# &&
-    		#ln -nfs #{shared_path}/public/assets/themes/ #{current_path}/public/assets/themes && 
-    		#ln -nfs #{shared_path}/public/uploads/themes/ #{current_path}/public/uploads/themes && 
-    		#ln -nfs #{shared_path}/public/uploads/photo/ #{current_path}/public/uploads/photo
-    	
-  	end
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml && ln -nfs #{shared_path}/public/assets/themes/ #{current_path}/public/assets/themes && ln -nfs #{shared_path}/public/uploads/themes/ #{current_path}/public/uploads/themes && ln -nfs #{shared_path}/public/uploads/photo/ #{current_path}/public/uploads/photo"	
+	end
 
 	task :start do ; end
 	task :stop do ; end
 	task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
+  exceptnd
 end
 
 before 'bundle:install', 'deploy:symlink_shared'
