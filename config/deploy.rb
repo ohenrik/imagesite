2f42787f481c0c20289138640947b0f623d1240f
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2014-03-25 20:03:28 +0100

    Removed folders, trying to get symlinks to work

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,15 +30,16 @@ after "deploy:restart", "deploy:cleanup"
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :symlink_shared do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/public/assets/themes #{release_path}/public/assets/themes"
-    run "ln -nfs #{shared_path}/public/uploads/themes #{release_path}/public/uploads/themes"
-    run "ln -nfs #{shared_path}/public/uploads/photo #{release_path}/public/uploads/photo"
-  end
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"# &&
+    		#ln -nfs #{shared_path}/public/assets/themes/ #{current_path}/public/assets/themes && 
+    		#ln -nfs #{shared_path}/public/uploads/themes/ #{current_path}/public/uploads/themes && 
+    		#ln -nfs #{shared_path}/public/uploads/photo/ #{current_path}/public/uploads/photo
+    	
+  	end
 
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
+	task :start do ; end
+	task :stop do ; end
+	task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
