074780aa7f09f3627744cb5e332f297eac81ecf4
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2014-03-26 21:02:45 +0100

    Removed Theme Upload button and fixed theme upload design (now using modal).

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -37,10 +37,18 @@ class ThemesController < ApplicationController
   # GET /themes/new
   def new
     @theme = Theme.new
+    respond_to do |format|
+        format.html {  }
+        format.js { render template: 'themes/edit' }
+    end
   end
 
   # GET /themes/1/edit
   def edit
+    respond_to do |format|
+        format.html {  }
+        format.js {  }
+    end 
   end
 
   # POST /themes
