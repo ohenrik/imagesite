b82f8d888499725829a22ef2544d2ac9b41725c9
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2014-06-08 13:29:39 +0200

    Added people show to guest permissions

--- app/models/permissions/guest_permission.rb
+++ app/models/permissions/guest_permission.rb
@@ -1,20 +1,33 @@
 module Permissions
 	class GuestPermission < BasePermission
 		def initialize()
-			allow :static_pages, [:home, :navigation, :user_home]
+			# People
+			allow :people, [:show]
+
+			# Pages
+			allow :pages, [:show]
+
+			# Photos
+			allow :photos, [:index, :show]
+
+			# Users
 			allow :users, [:new, :create, :home, :confirm_user]
+			allow_param :user, [:first_name, :last_name, :username, :subdomain, :email, :password, :password_confirmation]
+
+			# Sessions and user resets
+			allow :sessions, [:new, :create, :destroy]
 			allow :password_resets, [:new, :create, :update, :edit]
 			allow_param :password_reset, [:email, :password, :password_confirmation]
-			allow_param :user, [:first_name, :last_name, :username, :subdomain, :email, :password, :password_confirmation]
-			allow :sessions, [:new, :create, :destroy]
-			allow :photos, [:index, :show]
+
+			# Tags
 			allow :tags, [:show]
-			allow :pages, [:show]
+
+			# Static pages
+			allow :static_pages, [:home, :navigation, :user_home]
+
+			# Code Files
 			allow :code_files, [:show]
-			allow :public_code_files, [:show, :index, :edit, :update, :new, :create, :delete, :destroy]
-			allow :public_themes, [:index, :show, :edit, :update, :new, :create, :delete, :destroy]
-			allow_param :public_theme, [:name, :thumbnail]
-			allow_param :public_code_file, [:theme_id, :name, :display_name, :code, :hierarchy, :created_at, :updated_at, :static_file, :available_in => [], :available_to => []]
+
 		end
 	end
 end
